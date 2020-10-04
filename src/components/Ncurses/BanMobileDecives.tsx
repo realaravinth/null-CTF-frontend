f530d28e9fe8fafeb3e09edd6ff29b2ef6888d3e
realaravinth <realaravinth@batsense.net>
2020-10-04 22:35:05 +0530

    added styling to menudialogbig

--- src/components/Ncurses/BanMobileDecives.tsx
+++ src/components/Ncurses/BanMobileDecives.tsx
@@ -13,8 +13,11 @@ const BanMobileDevices: React.FunctionComponent = () => (
 		topText={"Mobile devices are not supported. We regret the inconvenience caused."}
 		pageTitle={"Error"} >
 		<WithContentContainer>
-			 <p> This CTF relies heavily on the functionality provided by
-			 desktop computers so please visit us from a desktop computer </p>
+			<p> 
+				This CTF relies heavily on the functionality provided by 
+				browsers on desktop computers so please visit us from a desktop
+				computer
+			</p>
 		</WithContentContainer>
 	</WithMenuDialog>
 );
