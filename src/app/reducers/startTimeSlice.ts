ca4cd1c3b2f68b0cc6a5ddd7d9c5aa5c8957722f
realaravinth <realaravinth@batsense.net>
2020-10-17 21:05:19 +0530

    redux types and login logic

---
+++ src/app/reducers/startTimeSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'; 
+
+import  State  from '../state';
+
+export interface startTimeState {
+	value: Time
+}
+
+enum Time {
+	null,
+	number
+}
+
+export const startTimeSlice = createSlice({
+  name: 'startTime',
+  initialState: {
+    value: Time.null,
+  },
+  reducers: {
+	  set_start_time: (state, action: PayloadAction<number>) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { set_start_time } = startTimeSlice.actions;
+
+export const selectstartTime = (state: State) => state.startTime.value;
+
+export default startTimeSlice.reducer;
